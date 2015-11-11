c3479d356b9a0d0047f5e2997f033eba1482f836
Benjamin Cable <cable.benjamin@gmail.com>
2015-11-11 04:47:07 +0100

    corrections to mobile menu, min-height for menubar on desktop size to avoid messing up on scroll animation
    Signed-off-by: Benjamin Cable <cable.benjamin@gmail.com>

--- js/app.js
+++ js/app.js
@@ -23,21 +23,31 @@ function toggleDropdown() { // jshint ignore: line
 }
 
 // Header styles
+$(window).on('load', function() {
+  "use strict";
+  var header = $(".navbar");
+  var scroll = $(window).scrollTop();
+  if (scroll >= 300) {
+    header.removeClass('transparent-header').addClass("full-header");
+  } else {
+    header.removeClass("full-header").addClass('transparent-header');
+  }
+});
+
 $(function() {
   "use strict";
-    //caches a jQuery object containing the header element
-    var header = $(".navbar");
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
 
-        if (scroll >= 300) {
-            header.removeClass('transparent-header').addClass("full-header");
-        } else if (scroll == 0) {
-            header.addClass('transparent-header');
-        } else {
-            header.removeClass("full-header").addClass('transparent-header');
-        }
-    });
+  //caches a jQuery object containing the header element
+  var header = $(".navbar");
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 300) {
+      header.removeClass('transparent-header').addClass("full-header");
+    } else {
+      header.removeClass("full-header").addClass('transparent-header');
+    }
+  });
 });
 
 // Form submission "thank you" note
